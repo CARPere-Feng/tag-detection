8ed49f2094dd85577d5f45861ad66391d7714e61
HanwenCao <caohw9@hotmail.com>
2021-09-24 14:51:01 +0800

    Add ArUco and AprilTag

---
+++ ArUco detection/build/CMakeFiles/auto_market_ppe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auto_market_ppe.dir/src/handeyeTarget.cpp.o"
+  "libauto_market_ppe.pdb"
+  "libauto_market_ppe.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/auto_market_ppe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ArUco detection/build/CMakeFiles/testHandeyeTarget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testHandeyeTarget.dir/test/handeyeTargetTest.cpp.o"
+  "testHandeyeTarget"
+  "testHandeyeTarget.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testHandeyeTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
